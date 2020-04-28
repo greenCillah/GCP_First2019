e7d2d894786e6f31ce6fd62bc033d584e8f377b6
Paul Green <pg361971@crownresorts.com.au>
2020-04-28 20:52:51 +1000

    get records

--- source/dataAccess/dataAccess.js
+++ source/dataAccess/dataAccess.js
@@ -6,8 +6,29 @@ module.exports = {
     foo: function () {
         return(false);
       },
-      getRecords: function (){
-        return("123456");
+    getRecords: function (){
+      let dataTxt;  
+      
+      let citiesRef = db.collection('users');
+        let query = citiesRef.where('last', '==', true).get()
+          .then(snapshot => {
+        
+          if (snapshot.empty) {
+            console.log('No matching documents.');
+            return;
+          }  
+
+          snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+
+            dataTxt = dataTxt + "___" + doc.data();
+          });
+        })
+        .catch(err => {
+          console.log('Error getting documents', err);
+         });
+
+        return(dataTxt);
       }
     };
 
